8e2ad5ac010844014d65fd966fcb127356621947
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2021-09-14 15:48:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+<link href="//dl.dropbox.com/s/50g6fbds3rh4m0j/saturnicons.css" rel="stylesheet"><script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i" rel="stylesheet"><link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i" rel="stylesheet"><!------------------------- SCRIPTS! DONT TOUCH! -------------------------><!-- photosets --><link href="https://static.tumblr.com/qudkd6d/OcDnl99gb/style.css" rel="stylesheet" type="text/css"/><script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script><script src="https://static.tumblr.com/qudkd6d/Az6nkemqr/pxuphotoset.min.js"></script><script>
+$(document).ready(function(){
+   $('.photo-slideshow').pxuPhotoset({
+       lightbox: true,
+       rounded: true,
+       gutter: '4px',
+       borderRadius: '5px',
+       photoset: '.photo-slideshow',
+       photoWrap: '.photo-data',
+       photo: '.pxu-photo'
+   });
+});
+</script><!-- tooltips --><script src="https://static.tumblr.com/rzl30kg/eAxm7a751/jquery.style-my-tooltips.js"></script><script>
+   (function($){
+       $(document).ready(function(){
+           $("[title]").style_my_tooltips({
+               tip_follows_cursor:true,
+               tip_delay_time:200,
+               tip_fade_speed:300
+           }
+               );
+       });
+   })(jQuery);
+</script><!-- smooth scroll --><script src="https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.1/SmoothScroll.min.js"></script>
